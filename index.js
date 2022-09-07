76b74ebd277aa2bc9152f7313ce0be6e0368228c
Will Martins <100540665+Martinswillians@users.noreply.github.com>
2022-09-07 18:01:30 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
+
+const cliente1 = new Cliente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
+
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
+
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
+
+contaCorrenteRicardo.depositar(200);
+contaCorrenteRicardo.depositar(100);
+
+
+const valorSacado = contaCorrenteRicardo.sacar(70);
+const valorDepositado = contaCorrenteRicardo.depositar(200);
+
+
+
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 1002;
+
+conta2.depositar(50);
+conta2.depositar(100);
+
+const valorSacado2 = conta2.sacar(50);
+
+contaCorrenteRicardo.tranferir(200, conta2);
+
+
+console.log(valorSacado);
+console.log(valorDepositado);
+
+console.log(contaCorrenteRicardo);
+
+console.log(cliente2);
+console.log(valorSacado2);
+console.log(conta2);
