11d64e740f0cb413049d6b873fb9e1c5e05f77c7
Will Martins <100540665+Martinswillians@users.noreply.github.com>
2022-09-07 19:50:07 -0300

    Add files via upload

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,6 +1,7 @@
 import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente{
+    static numeroDeContas = 0;
     agencia;
     _cliente;
 
@@ -27,7 +28,7 @@ export class ContaCorrente{
     constructor(agencia, cliente){
         this.agencia = agencia;
         this.cliente = cliente;
-
+        ContaCorrente.numeroDeContas += 1;
     }
 
     sacar(valor){
--- index.js
+++ index.js
@@ -2,9 +2,7 @@ import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
-
 const cliente2 = new Cliente("Alice", 88822233309);
-
 
 
 const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
@@ -16,16 +14,12 @@ contaCorrenteRicardo.depositar(100);
 const valorSacado = contaCorrenteRicardo.sacar(70);
 const valorDepositado = contaCorrenteRicardo.depositar(200);
 
-
-
 const conta2 = new ContaCorrente(1002, cliente2);
-
 
 conta2.depositar(50);
 conta2.depositar(100);
 
 const valorSacado2 = conta2.sacar(50);
-
 contaCorrenteRicardo.tranferir(200, conta2);
 
 
@@ -35,3 +29,5 @@ console.log(contaCorrenteRicardo);
 
 console.log(valorSacado2);
 console.log(conta2);
+
+console.log("Atualmente existem " + ContaCorrente.numeroDeContas + " novas contas!");
